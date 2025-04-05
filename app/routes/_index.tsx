4c1d1afe27b93a922a720cb7a6da5837a7e95017
stefvidmar <stefano@doneros.co>
2025-04-06 01:12:40 +0200

    feat:watch a demo section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "~/components/global/navbar";
 import Hero from "~/components/marketing/hero";
+import VideoDemo from "~/components/marketing/video-demo";
 
 export default function Page() {
   return (
     <>
     <Navbar />
     <Hero/>
+    <VideoDemo/>
     </>
   );
 }
