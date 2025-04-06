ebb6822197afdaa073eba86269a657f263043b4e
stefvidmar <stefano@doneros.co>
2025-04-06 12:44:19 +0200

    feat: dashboard sidebar + dashboard Breadcrumbs + Dashboard Layout

--- app/routes/_dashboard.tsx
+++ app/routes/_dashboard.tsx
@@ -1,11 +1,31 @@
 import { Outlet } from "react-router";
+import { AppSidebar } from "~/components/global/admin/app-sidebar";
+import { Breadcrumbs } from "~/components/global/admin/breadcrumbs";
+import { Separator } from "~/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Dashboard Layout</h1>
-      <Outlet />
-    </div>
+    <SidebarProvider
+    style={
+      {
+        '--sidebar-width': '19rem',
+      } as React.CSSProperties
+    }
+  >
+    <AppSidebar variant="inset" />
+    <SidebarInset>
+      <div className="flex flex-1 flex-col p-4">
+        {/* BreadCrumbs Component */}
+        <div className="flex items-center gap-2 pb-4 md:pb-10">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-2" />
+            <Breadcrumbs/>
+          </div>
+        <Outlet/>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
   );
 }
 
