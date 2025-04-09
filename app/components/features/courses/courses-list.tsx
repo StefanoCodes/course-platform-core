9d14512f7efcc7f378208106a8d228d4831c69e7
stefvidmar <stefano@doneros.co>
2025-04-09 13:29:46 +0200

    feat: enhance course management with delete functionality
    - Refactored CoursesList component to use a new CourseCard component for better organization and readability.
    - Added DeleteCourse component to allow course deletion directly from the course card.
    - Updated MarkAsPublic and MarkAsPrivate components to change the submission state handling.
    - Implemented server-side function to handle course deletion, including cascading deletion of associated segments.
    - Updated resource route to include delete course action.

--- app/components/features/courses/courses-list.tsx
+++ app/components/features/courses/courses-list.tsx
@@ -6,46 +6,54 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { useCoursesLoaderData } from "~/routes/_dashboard.dashboard.courses";
 import { MarkAsPublic } from "./mark-as-public";
 import { MarkAsPrivate } from "./mark-as-private";
+import type { Course } from "~/db/schema";
+import { DeleteCourse } from "./delete-course";
 
 export function CoursesList() {
     const { courses } = useCoursesLoaderData();
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {courses.map(({ id, name, description, isPublic, slug }) => {
-                return (
-                    <Card key={id}>
-                        <CardHeader className="flex flex-row justify-between items-center">
-                            <CardTitle>
-                                <h3>
-                                    {name}
-                                </h3>
-                            </CardTitle>
-
-                            {isPublic ? <IsPublicBadge /> : <IsPrivateBadge />}
-
-                        </CardHeader>
-                        <CardContent>
-                            <CardDescription>
-                                {description}
-                            </CardDescription>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-between items-center">
-                            <Button variant="outline" asChild>
-                                <Link to={`/dashboard/courses/${slug}`}>
-                                    <Pencil />
-                                    Edit Course
-                                </Link>
-                            </Button>
-                            {!isPublic ? <MarkAsPublic courseId={id} /> : <MarkAsPrivate courseId={id} />}
-                        </CardFooter>
-                    </Card>
-                )
-            })}
-
+            {courses.map((course) => (
+                <CourseCard key={course.id} course={course} />
+            )
+            )}
         </div>
     )
 }
+function CourseCard({ course }: { course: Course }) {
+    const { id, name, description, isPublic, slug } = course;
+    return (
+        <Card className="relative">
+            <CardHeader className="flex flex-row justify-between items-center">
+                <CardTitle>
+                    <h3>
+                        {name}
+                    </h3>
+                </CardTitle>
 
+                {isPublic ? <IsPublicBadge /> : <IsPrivateBadge />}
+
+            </CardHeader>
+            <CardContent>
+                <CardDescription>
+                    {description}
+                </CardDescription>
+            </CardContent>
+            <CardFooter className="flex flex-row justify-between items-center">
+                <Button variant="outline" asChild>
+                    <Link to={`/dashboard/courses/${slug}`}>
+                        <Pencil />
+                        Edit Course
+                    </Link>
+                </Button>
+                {!isPublic ? <MarkAsPublic courseId={id} /> : <MarkAsPrivate courseId={id} />}
+            </CardFooter>
+            <div className="absolute -top-4 -right-4">
+                <DeleteCourse courseId={id} />
+            </div>
+        </Card>
+    )
+}
 function IsPublicBadge() {
     return (
         <Badge className="flex bg-brand-primary flex-row items-center gap-1">
@@ -54,7 +62,6 @@ function IsPublicBadge() {
         </Badge>
     )
 }
-
 function IsPrivateBadge() {
     return (
         <Badge variant="default" className="flex flex-row items-center gap-1">
