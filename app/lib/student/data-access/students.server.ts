e429c39e64f8f1de9bafa91b84cd71c19d851968
stefvidmar <stefano@doneros.co>
2025-04-10 22:18:25 +0200

    fix: handle undefined student in account activation check
    - Added a check to return false for isStudentActivated if the student is undefined, ensuring proper handling when an admin accesses the function.

--- app/lib/student/data-access/students.server.ts
+++ app/lib/student/data-access/students.server.ts
@@ -8,8 +8,11 @@ export async function isStudentAccountActivated(email: string) {
 
     // get active status for a student based on their id
     const [student] = await db.select().from(studentsTable).where(eq(studentsTable.email, email));
+    // incase the admin ends up here the student will be undefined so we just return false if thats the case
+    if (!student) return {
+        isStudentActivated: false,
+    }
     return {
         isStudentActivated: student.isActivated,
     }
-
 }
